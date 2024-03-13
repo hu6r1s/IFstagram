11a6264c783055270af9d955ca55c31fdeef985a
Hu6r1s <darmaa333@gmail.com>
2024-03-13 17:39:13 +0900

    :recycle: refactor(image): image upload and read by S3

--- src/main/java/com/nbcampif/ifstagram/domain/image/service/PostImageService.java
+++ src/main/java/com/nbcampif/ifstagram/domain/image/service/PostImageService.java
@@ -3,27 +3,49 @@ package com.nbcampif.ifstagram.domain.image.service;
 import com.nbcampif.ifstagram.domain.image.entity.PostImage;
 import com.nbcampif.ifstagram.domain.image.repository.PostImageRepository;
 import com.nbcampif.ifstagram.domain.post.entity.Post;
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Exception;
+import io.awspring.cloud.s3.S3Operations;
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.Response;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectAclResponse;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectAclRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class PostImageService {
 
   @Value("${upload.path}")
   private String uploadPath;
 
-  private final PostImageRepository postImageRepository;
+  @Value("${cloud.aws.s3.bucket}")
+  private String bucket;
 
+  private final S3Client s3Client;
+
+  private final PostImageRepository postImageRepository;
 
   @Transactional
   public void createImage(MultipartFile imageFile, Post post) throws Exception {
@@ -33,11 +55,19 @@ public class PostImageService {
 
 
   public String getImage(Long id) throws MalformedURLException {
-    Optional<PostImage> image = postImageRepository.findById(id);
-    if (!image.isPresent()) {
-      return "";
+    PostImage image = postImageRepository.findById(id).orElse(null);
+
+    if (image == null || image.getFilePath() == null) {
+       return null;
     }
-    return image.get().getFilePath();
+
+    GetUrlRequest request = GetUrlRequest
+      .builder()
+      .bucket(bucket)
+      .key(image.getFilePath())
+      .build();
+
+    return s3Client.utilities().getUrl(request).toExternalForm();
   }
 
   @Transactional
@@ -48,31 +78,41 @@ public class PostImageService {
     postImage.updatePostImage(image);
   }
 
-  private PostImage getPostImage(MultipartFile file, Post post) throws IOException {
-    if (file != null) {
+  private PostImage getPostImage(MultipartFile file, Post post) {
+    try {
       String originalFilename = file.getOriginalFilename();
+      String extension = StringUtils.getFilenameExtension(originalFilename);
       String saveFileName = createSaveFileName(originalFilename);
-      file.transferTo(new File(getFullPath(saveFileName)));
-      String filePath = uploadPath + saveFileName;
+      PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+        .bucket(bucket)
+        .key(uploadPath + saveFileName)
+        .contentType(extension)
+        .build();
+      PutObjectResponse response = s3Client
+        .putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
 
+      String filePath = uploadPath + saveFileName;
       String contentType = file.getContentType();
 
-      PostImage image = PostImage.builder()
-        .fileName(originalFilename)
-        .saveFileName(saveFileName)
-        .contentType(contentType)
-        .filePath(filePath)
-        .post(post)
-        .build();
-      return image;
+      if(response.sdkHttpResponse().statusText().orElse("FAIL").equals("OK")){
+        PostImage image = PostImage.builder()
+          .fileName(originalFilename)
+          .saveFileName(saveFileName)
+          .contentType(contentType)
+          .filePath(filePath)
+          .post(post)
+          .build();
+        return image;
+      }else{
+        throw new IllegalStateException("AWS에 파일을 올리는데 실패했습니다.");
+      }
+    } catch(IOException ie){
+      throw new RuntimeException(ie.getMessage());
+    } catch (S3Exception ae){
+      throw new RuntimeException(ae.getMessage());
+    } catch (IllegalStateException se){
+      throw new RuntimeException(se.getMessage());
     }
-    return PostImage.builder()
-      .fileName(null)
-      .saveFileName(null)
-      .contentType(null)
-      .filePath(null)
-      .post(post)
-      .build();
   }
 
   private String createSaveFileName(String originalFilename) {
